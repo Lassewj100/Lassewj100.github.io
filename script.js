5f6e2324c57bb6e8bfa36a0e1cba99e49ba785cb
Lassewj100 <lassejohnsgard@gmail.com>
2026-05-29 09:24:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function oppdater() {
     var tid = new Date();
-    var hours = tid.getHours().toString().padStart(2, '0');
-    var minutes = tid.getMinutes().toString().padStart(2, '0');
-    var seconds = tid.getSeconds().toString().padStart(2, '0');
+    var timer = tid.getHours().toString().padStart(2, '0');
+    var minnuter = tid.getMinutes().toString().padStart(2, '0');
+    var sekunder = tid.getSeconds().toString().padStart(2, '0');
     var timeString = hours + ":" + minutes + ":" + seconds;
     
     document.getElementById("klokkeutskrift").innerHTML = timeString;
